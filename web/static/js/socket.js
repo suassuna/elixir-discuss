77ec8746047acf319e812052d693b5ea326c1460
suassuna <suassuna.paulo@gmail.com>
2018-09-02 19:03:53 -0300

    socket script must be loaded only when specified

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -53,10 +53,13 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (topicId) => {
+  // Now that you are connected, you can join channels with a topic:
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+}
+
+window.createSocket = createSocket;
