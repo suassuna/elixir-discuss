d5518badb43490799379cba633bf1650766cd9f2
suassuna <suassuna.paulo@gmail.com>
2018-09-03 09:02:50 -0300

    Added comment box and button to topic show page

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,12 +1,16 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
+  alias Discuss.Topic
 
-  def join(name, _params, socket) do
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
-  def handle_in(name, message, socket) do    
+  def handle_in(name, %{ "content" => content }, socket) do
+    IO.inspect(content)
     {:reply, :ok, socket}
   end
 end
